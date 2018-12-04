32cd92f1dabe879c6806da9aa55cb66d95201f11
진현영 <nb90592@navercorp.com>
2018-12-04 10:20:29 +0900

    최초 커밋

---
+++ SpringExample/src/main/java/com/spring/myapp/dao/impl/BoardDaoImpl.java
@@ -0,0 +1,110 @@
+package com.spring.myapp.dao.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.spring.myapp.dao.BoardDao;
+import com.spring.myapp.domain.Board;
+import com.spring.myapp.domain.BoardReply;
+
+@Repository("boardDao")
+public class BoardDaoImpl implements BoardDao{
+	
+	@Autowired
+    private SqlSession sqlSession;
+ 
+    public void setSqlSession(SqlSession sqlSession){
+        this.sqlSession = sqlSession;
+    }
+    
+	@Override
+	public int regContent(Map<String, Object> paramMap) {
+    	System.out.println(paramMap.get("subject").toString());
+	System.out.println(paramMap.get("writer").toString());
+	System.out.println(paramMap.get("password").toString());
+	System.out.println(paramMap.get("content").toString());
+		
+		return sqlSession.insert("insertContent", paramMap);
+	}
+	
+	@Override
+	public int modifyContent(Map<String, Object> paramMap) {
+		return sqlSession.update("updateContent", paramMap);
+	}
+
+	@Override
+	public int getContentCnt(Map<String, Object> paramMap) {
+		return sqlSession.selectOne("selectContentCnt", paramMap);
+	}
+	
+	@Override
+	public List<Board> getContentList(Map<String, Object> paramMap) {
+		return sqlSession.selectList("selectContent", paramMap);
+	}
+
+	@Override
+	public Board getContentView(Map<String, Object> paramMap) {
+		return sqlSession.selectOne("selectContentView", paramMap);
+	}
+
+	@Override
+	public int regReply(Map<String, Object> paramMap) {
+		return sqlSession.insert("insertBoardReply", paramMap);
+	}
+
+	@Override
+	public List<BoardReply> getReplyList(Map<String, Object> paramMap) {
+		return sqlSession.selectList("selectBoardReplyList", paramMap);
+	}
+
+	@Override
+	public int delReply(Map<String, Object> paramMap) {
+		if(paramMap.get("r_type").equals("main")) {
+			//�θ���� ���� �� ����
+			return sqlSession.delete("deleteBoardReplyAll", paramMap);
+		}else {
+			//�ڱ� �ڽŸ� ����
+			return sqlSession.delete("deleteBoardReply", paramMap);
+		}
+		
+		
+	}
+
+	@Override
+	public int getBoardCheck(Map<String, Object> paramMap) {
+		return sqlSession.selectOne("selectBoardCnt", paramMap);
+	}
+
+	@Override
+	public int delBoard(Map<String, Object> paramMap) {
+		return sqlSession.delete("deleteBoard", paramMap);
+	}
+
+	@Override
+	public boolean checkReply(Map<String, Object> paramMap) {
+		int result = sqlSession.selectOne("selectReplyPassword", paramMap);
+				
+		if(result>0) {
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
+
+	@Override
+	public boolean updateReply(Map<String, Object> paramMap) {
+		int result = sqlSession.update("updateReply", paramMap);
+		
+		if(result>0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+}
